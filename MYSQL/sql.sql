3115e73eeccdfd1e758582369a1a7eeb96baa9b7
Ahmed <a@a.com>
2018-02-22 23:43:34 +0300

    upload code SQL

--- MYSQL/sql.sql
+++ MYSQL/sql.sql
@@ -1,16 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 22, 2018 at 11:40 PM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Database: `projectphpdb`
 --
-create database projectphpdb;
-use projectphpdb;
+DROP DATABASE IF EXISTS `projectphpdb`;
+CREATE DATABASE IF NOT EXISTS `projectphpdb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `projectphpdb`;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emp`
+--
+
+DROP TABLE IF EXISTS `emp`;
+CREATE TABLE `emp` (
+  `ID` int(11) NOT NULL,
+  `NAME` varchar(30) NOT NULL,
+  `PHONE` varchar(30) NOT NULL,
+  `EMAIL` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `emp`
+--
+
+INSERT INTO `emp` (`ID`, `NAME`, `PHONE`, `EMAIL`) VALUES
+(1, 'emp01', '123456789', 'a@a.com'),
+(2, 'emp02', '123456789', 'a@'),
+(3, 'emp03', '123456789', 'a@a.com'),
+(4, 'emp04', '123456789', 'a@'),
+(5, 'emp05', '123456789', 'a@a.com'),
+(6, 'emp06', '123456789', 'a@');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+
+DROP TABLE IF EXISTS `jobs`;
 CREATE TABLE `jobs` (
   `ID` int(11) NOT NULL,
   `NAME` varchar(20) DEFAULT 'unknown'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `emp`
+--
+ALTER TABLE `emp`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `jobs`
+--
 ALTER TABLE `jobs`
   ADD PRIMARY KEY (`ID`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `emp`
+--
+ALTER TABLE `emp`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `jobs`
+--
 ALTER TABLE `jobs`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
